5430f8a0e384ee1979ed63fb54a6c3adfb45e0df
Josh Justice <me@codingitwrong.com>
2024-02-21 18:06:16 -0500

    Fill the window

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
 
 void main() => runApp(const MyApp());
 
@@ -40,167 +41,227 @@ class _CalculatorHomePageState extends State<CalculatorHomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Card(
-            color: Colors.lightGreen[50],
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                _str,
-                textScaler: const TextScaler.linear(2.0),
+          Expanded(
+            flex: 2,
+            child: Card(
+              color: Colors.lightGreen[50],
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  _str,
+                  textScaler: const TextScaler.linear(2.0),
+                ),
               ),
             ),
           ),
-          Row(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  deleteAll();
-                },
-                child: const Text('C'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  deleteOne();
-                },
-                child: const Text('<-'),
-              ),
-            ],
+          Expanded(
+            flex: 1,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      deleteAll();
+                    },
+                    child: const Text('C'),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      deleteOne();
+                    },
+                    child: const Text('<-'),
+                  ),
+                ),
+              ],
+            ),
           ),
-          Row(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Column(
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+          Expanded(
+            flex: 4,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          add('7');
-                        },
-                        child: const Text('7'),
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('7');
+                                },
+                                child: const Text('7'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('8');
+                                },
+                                child: const Text('8'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('9');
+                                },
+                                child: const Text('9'),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('8');
-                        },
-                        child: const Text('8'),
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('4');
+                                },
+                                child: const Text('4'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('5');
+                                },
+                                child: const Text('5'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('6');
+                                },
+                                child: const Text('6'),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('9');
-                        },
-                        child: const Text('9'),
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('1');
+                                },
+                                child: const Text('1'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('2');
+                                },
+                                child: const Text('2'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('3');
+                                },
+                                child: const Text('3'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('0');
+                                },
+                                child: const Text('0'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  add('.');
+                                },
+                                child: const Text('.'),
+                              ),
+                            ),
+                            Expanded(
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  getResult();
+                                },
+                                child: const Text('='),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          add('4');
-                        },
-                        child: const Text('4'),
+                      Expanded(
+                        child: ElevatedButton(
+                          child: Image.asset(
+                            "icons/divide.png",
+                            width: 10.0,
+                            height: 10.0,
+                          ),
+                          onPressed: () {
+                            add('÷');
+                          },
+                        ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('5');
-                        },
-                        child: const Text('5'),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            add('x');
+                          },
+                          child: const Text('x'),
+                        ),
                       ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('6');
-                        },
-                        child: const Text('6'),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            add('-');
+                          },
+                          child: const Text('-'),
+                        ),
+                      ),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            add('+');
+                          },
+                          child: const Text('+'),
+                        ),
                       ),
                     ],
                   ),
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          add('1');
-                        },
-                        child: const Text('1'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('2');
-                        },
-                        child: const Text('2'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('3');
-                        },
-                        child: const Text('3'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          add('0');
-                        },
-                        child: const Text('0'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          add('.');
-                        },
-                        child: const Text('.'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          getResult();
-                        },
-                        child: const Text('='),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                // crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ElevatedButton(
-                    child: Image.asset(
-                      "icons/divide.png",
-                      width: 10.0,
-                      height: 10.0,
-                    ),
-                    onPressed: () {
-                      add('÷');
-                    },
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      add('x');
-                    },
-                    child: const Text('x'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      add('-');
-                    },
-                    child: const Text('-'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      add('+');
-                    },
-                    child: const Text('+'),
-                  ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
